0368d949f4b42a741570ec80d902605707261f58
Nate <natemckenzierp@gmail.com>
2026-09-22 18:59:09 -0300

    Add new allowed origin for Shopify API

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(session({
 }));
 
 // Solo tu tienda de Shopify puede llamar a esta API.
-const ALLOWED_ORIGINS = ['https://valleydreamsarg.myshopify.com', 'https://n1bc3b-ba.myshopify.com', 'https://valleydreams.com.ar'];
+const ALLOWED_ORIGINS = ['https://valleydreamsarg.myshopify.com', 'https://n1bc3b-ba.myshopify.com', 'https://valleydreams.com.ar', 'https://solzen.com.ar'];
 
 const corsOptions = {
   origin(origin, callback) {
